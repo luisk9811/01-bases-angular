05d5156e873dfad60c219f0344f0b7808c713526
Luis Carlos Rodriguez Diaz <luiscrd9811@gmail.com>
2022-02-04 15:51:56 -0500

    Segundo Commit

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  // agregarNuevoPersonaje(argumento:Personaje){
+  //   this.personajes.push(argumento)
+  // }
+
+  constructor(){}
+
+}
